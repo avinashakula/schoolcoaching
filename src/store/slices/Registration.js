9b35b3cedb4eb8e6965969e0bbb5df0e9cdb7406
Sri-1995 <srisrinivasa7689@gmail.com>
2023-11-20 12:25:30 +0530

    jwt authenticated code

--- src/store/slices/Registration.js
+++ src/store/slices/Registration.js
@@ -1,6 +1,54 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const newUserAsync = createAsyncThunk('register/newUserAsync', async (user) => {
+    // For subsequent Requests
+    // const token = localStorage.getItem('jwtToken');
+    // if( !token ){
+    //     return {"status":false, "msg":"User not authenticated"}
+    // }else{
+    //     const response = await fetch('http://desireitservices.in/schoolcoaching/server/profile.php', {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //         },
+    //     });
+
+    //     if (response.ok) {
+    //         // Handle successful response
+    //         const data = await response.json();
+    //         console.log('Data:', data);
+    //     } else {
+    //         // Handle unauthorized or other errors
+    //         console.error('Request failed');
+    //     }
+    // }
+
+
+    // For Login
+    // const token = localStorage.getItem('jwtToken');   
+    // const response = await fetch('http://desireitservices.in/schoolcoaching/server/login.php', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ username, password }),
+    // });
+
+    // if (response.ok) {
+    //     const data = await response.json();
+    //     const token = data.token;
+
+    //     // Store the token in localStorage
+    //     localStorage.setItem('jwtToken', token);
+    //     console.log('Login successful');
+    // } else {
+    //     console.error('Login failed');
+    // }
+
+    // For Logout
+    // localStorage.removeItem('jwtToken');
+
+
+    //For Registration 
     const config = {
         method: 'POST',
         headers: {
@@ -26,6 +74,7 @@ export const RegistrationSlice = createSlice({
     name:"register",
     initialState: {
         users:[],
+        usersList:[],
         loading:false,
         msg:""
     },
@@ -58,6 +107,7 @@ export const RegistrationSlice = createSlice({
         builder.addCase(newUserAsync.fulfilled, (state, action) => {           
             state.loading = false;
             state.msg = action.payload.msg
+            localStorage.setItem("jwtToken", action.payload.token)
         });
 
         // Handle the rejected (error) state for the async thunk
